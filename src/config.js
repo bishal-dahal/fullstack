9ce273ec08c0007c0e895507cbf5f8593e9169c5
bsalsoko <101317575+bsalsoko@users.noreply.github.com>
2023-07-03 18:23:02 +0545

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-export const TRANSLATION_LANGUAGES = ['fi', 'en', 'zh', 'es', 'fr', 'ptbr'];
+export const TRANSLATION_LANGUAGES = [ 'en', 'fi', 'zh', 'es', 'fr', 'ptbr'];
 
 export const TRANSLATION_LANGUAGE_OPTIONS = [
-  { value: 'fi', label: 'Suomi' },
   { value: 'en', label: 'English' },
+  { value: 'fi', label: 'Suomi' },
   { value: 'zh', label: '中文' },
   { value: 'es', label: 'Español' },
   { value: 'fr', label: 'Français' },
